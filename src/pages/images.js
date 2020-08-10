a683b8eb4e683bdd5f413160d39cbce8eedcc944
Subha Velaga <subha.velaga@gmail.com>
2020-08-10 17:06:44 -0500

    changed image names

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,45 +1,45 @@
-import img1 from "../../dist/images/1.jpg";
-import img2 from "../../dist/images/2.jpg";
-import img3 from "../../dist/images/3.jpg";
-import img4 from "../../dist/images/4.jpg";
-import img5 from "../../dist/images/5.jpg";
-import img6 from "../../dist/images/Dolly.jpg";
+import img1 from "../../dist/images/doggo.jpg";
+import img2 from "../../dist/images/cyclist.jpg";
+import img3 from "../../dist/images/dancer.jpg";
+import img4 from "../../dist/images/guitarist.jpg";
+import img5 from "../../dist/images/beach.jpg";
+import img6 from "../../dist/images/dolly.jpg";
 
 export const images = [
   {
     src: img1,
-    title: "this is the title",
-    description: "this is the description",
-    medium: "this is the medium"
+    title: "Dreamy doggo",
+    description: "Sam the doggo dreaming away",
+    medium: "oil on canvas 18X24"
   },
   {
     src: img2,
-    title: "this is the title",
-    description: "this is the description",
-    medium: "this is the medium"
+    title: "The morning cyclist",
+    description: "",
+    medium: "oil on canvas 18X24"
   },
   {
     src: img3,
-    title: "this is the title",
-    description: "this is the description",
-    medium: "this is the medium"
+    title: "Kuchipudi dancer",
+    description: "",
+    medium: "oil on canvas 24X30"
   },
   {
     src: img4,
-    title: "this is the title",
-    description: "this is the description",
-    medium: "this is the medium"
+    title: "The guitarist",
+    description: "",
+    medium: "oil on canvas 18X24"
   },
   {
     src: img5,
-    title: "this is the title",
-    description: "this is the description",
-    medium: "this is the medium"
+    title: "Cousins at the beach",
+    description: "",
+    medium: "oil on canvas 18X24"
   },
   {
     src: img6,
     title: "Dolly",
-    description: "this is the description",
+    description: "",
     medium: "oil on canvas 16X16"
   },
 ];
