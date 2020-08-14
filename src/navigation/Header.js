3ac3412518b90c8640f37dcb7b79d377ba1f28cd
Abhi Velaga <abhinav.velaga@gmail.com>
2020-08-13 23:17:57 -0500

    basic implementation done

--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -1,20 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 import './navigation.module.scss';
 
 export class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false
+    };
+  }
+
+  onClick = (event) => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
     return (
       <div className="header">
-      <div className="title-div">
-        <div className="title">Subhanjali</div>
-        <div className="description">My Art</div>
+
+        <div className="title-div">
+          <div className="title">Subhanjali</div>
+          <div className="description">My Art</div>
+        </div>
+
+        {/* desktop  */}
+        {!this.state.collapsed &&
+          <div className="nav">
+            <a href="https://medium.com/@subhanjali" target="_blank"><div className="link">Blog</div></a>
+            <a href="mailto:subha.velaga@gmail.com"><div className="link">Contact</div></a>
+            <a href="https://www.facebook.com/subhanjali" target="_blank"><div className="link">Facebook</div></a>
+          </div>
+        }
+
+        {/* mobile  */}
+        < MediaQuery maxDeviceWidth={500} >
+          <div className="menu-button" onClick={this.onClick}>+</div>
+        </ MediaQuery>
+        
       </div>
-      <div className="nav">
-      <a href="https://medium.com/@subhanjali" target="_blank"><div className="link">Blog</div></a>
-        <a href="mailto:subha.velaga@gmail.com"><div className="link">Contact</div></a>
-        <a href="https://www.facebook.com/subhanjali" target="_blank"><div className="link">Facebook</div></a>
-      </div>
-    </div>
     )
   }
 }
