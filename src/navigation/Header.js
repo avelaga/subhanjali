05665b7fa0ee228d00b987664c400319c928342d
Abhi Velaga <abhinav.velaga@gmail.com>
2020-08-14 10:45:32 -0500

    working menu transition

--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -6,15 +6,17 @@ export class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      collapsed: true
+      collapsed: true,
     };
   }
 
   onClick = (event) => {
     this.setState({
-      collapsed: !this.state.collapsed
+      collapsed: !this.state.collapsed,
     });
   }
+
+
 
   render() {
     return (
@@ -22,16 +24,23 @@ export class Header extends Component {
 
         {/* mobile  */}
         < MediaQuery maxDeviceWidth={500} >
-          <div className="menu-button" onClick={this.onClick}>+</div>
-          {!this.state.collapsed &&
-            <div className="nav">
-              <div className="menu-button" onClick={this.onClick}>-</div>
-              <a href="https://medium.com/@subhanjali" target="_blank"><div className="link">Blog</div></a>
-              <a href="mailto:subha.velaga@gmail.com"><div className="link">Contact</div></a>
-              <a href="https://www.facebook.com/subhanjali" target="_blank"><div className="link">Facebook</div></a>
-              <a href="https://www.linkedin.com/in/subhavelaga/" target="_blank"><div className="link">LinkedIn</div></a>
-            </div>
+
+          {this.state.collapsed &&
+            <div className="menu-button" onClick={this.onClick}>+</div>
           }
+          {!this.state.collapsed &&
+            <div className="menu-button" onClick={this.onClick}>-</div>
+          }
+
+          <div 
+          className={!this.state.collapsed ? "nav" : "show"} 
+          id="nav">
+            <a href="https://medium.com/@subhanjali" target="_blank"><div className="link">Blog</div></a>
+            <a href="mailto:subha.velaga@gmail.com"><div className="link">Contact</div></a>
+            <a href="https://www.facebook.com/subhanjali" target="_blank"><div className="link">Facebook</div></a>
+            <a href="https://www.linkedin.com/in/subhavelaga/" target="_blank"><div className="link">LinkedIn</div></a>
+          </div>
+
         </ MediaQuery>
 
         <div className="title-div">
